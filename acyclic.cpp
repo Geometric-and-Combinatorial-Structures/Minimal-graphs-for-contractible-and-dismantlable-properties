b5ba477c78d04d11105309d2b998c4070ca74960
hectorhdez2 <104125248+hectorhdez2@users.noreply.github.com>
2022-09-15 13:27:42 -0700

    Update acyclic.cpp

--- acyclic.cpp
+++ acyclic.cpp
@@ -9,8 +9,8 @@
      acyclic				: V and E will entered after
      acyclic V				: graphs with V vertices and E=V-1,V,V+1,..., V(V-1)/2 will be proceced
      acyclic V E			: graphs with V vertices and E edges wil be proceced 
-     acyclic V E n1			: graphs with V vertices, E edges and n1_th million  wil be proceced (by big files) 
-     acyclic V E n1 n2			: graphs with V vertices, E edges and n1_th to n1_th ( millions  wil be proceced  )
+     acyclic V E n1			: graphs with V vertices, E edges and n1_th million  will be proceced (by big files) 
+     acyclic V E n1 n2			: graphs with V vertices, E edges and n1_th to n1_th ( millions  will be proceced  )
 
 */
 
